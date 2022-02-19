bff2a3e56f0b67705b3ce3853c42a57c5b675b4c
Dusty McCord <Dustymccord@gmail.com>
2022-02-19 00:52:35 -0800

    Eval Guesses

--- utils/evalGuess.ts
+++ utils/evalGuess.ts
@@ -3,19 +3,34 @@ export const evalGuess = (
   masterWord: string,
   guess: LetterState[]
 ): LetterState[] => {
-  const rightPlace = guess.map((l, index) => {
-    if (
-      l.letter.toLocaleLowerCase() === masterWord[index].toLocaleLowerCase()
-    ) {
-      return { letter: l.letter, state: "correct" }
-    } else if (
-      masterWord.toLocaleLowerCase().includes(l.letter.toLocaleLowerCase())
-    ) {
-      return { letter: l.letter, state: "good" }
+  const masterTrimmed = [...masterWord.toLocaleLowerCase().split("")]
+  const correctResults = guess.map((l, idx) => {
+    if (l.letter.toLocaleLowerCase() === masterWord[idx].toLocaleLowerCase()) {
+      masterTrimmed[idx] = "!"
+      return {
+        letter: l.letter,
+        state: "correct",
+      }
+    } else {
+      return l
+    }
+  })
+  const withGoodButWrongSpace = correctResults.map((l, idx) => {
+    if (l.state === "correct") {
+      return l
+    } else if (masterTrimmed.includes(l.letter.toLocaleLowerCase())) {
+      const goodLetterIndex = masterTrimmed.indexOf(
+        l.letter.toLocaleLowerCase()
+      )
+      masterTrimmed[goodLetterIndex] = "!"
+      return {
+        state: "good",
+        letter: l.letter,
+      }
     } else {
       return l
     }
   })
   // @ts-ignore
-  return rightPlace
+  return withGoodButWrongSpace
 }
